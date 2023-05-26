ee955699acafd8fac128ac7971d03afce07eb01b
Vicki <46823403+VickiLanger@users.noreply.github.com>
2023-05-26 00:35:22 +0000

    Stylesheet component

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Styles.css";
 
-function Stylesheet() {
-  return <div></div>;
+function Stylesheet(props, { text }) {
+  let className = props.primary ? "primary" : "";
+  return (
+    <div>
+      <h3 class={`${className} cut-here`}>Conditional with multiple classes</h3>
+      <h3 class={`${className} cut-here`}>{props.text}</h3>
+    </div>
+  );
 }
 
 export default Stylesheet;
